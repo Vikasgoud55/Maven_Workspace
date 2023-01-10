957f967950ee6b65b5db51ddbcfd901cf399bb0a
vikas <vikas@vikas>
2023-01-10 11:22:19 +0530

    My first update

---
+++ Vikas_1A/src/test/java/p1/Class1.java
@@ -0,0 +1,20 @@
+package p1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1 {
+  @Test
+  public void f() 
+  {
+	  Reporter.log("Test1 executed");
+  }
+
+ public class class1a {
+ @Test           
+  public void f ()
+  {
+	 Reporter.log("Test1a executed");
+  }
+ }
+}
---
+++ Vikas_1A/src/test/java/p2/Class2.java
@@ -0,0 +1,19 @@
+package p2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2 {
+  @Test
+ public void f()
+  {
+	  Reporter.log("Test2 executed");
+  } 
+ public class class2a {
+		  @Test           
+		   public void f ()
+		   {
+		 	 Reporter.log("Test2a executed");
+		   }
+  }
+}
